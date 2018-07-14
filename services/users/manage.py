9f0f7f143cc7d6a7482481eb6ffb0ec47c4c1c2e
Chris Woo <chriskfwoo@gmail.com>
2018-07-13 21:00:40 -0400

    Setup docker with postgres

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,8 +1,15 @@
 from flask.cli import FlaskGroup
+from project import app,db
 
-from project import app
 
 cli = FlaskGroup(app)
+
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 if __name__ == '__main__':
     cli()
